f110c6364ff6dcfad3072cacf8837aa79a077972
lanlanliang <lanlanliang@tencent.com>
2024-07-24 20:01:44 +0800

    添加ppr配置

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,5 +1,7 @@
 import SideNav from '@/app/ui/dashboard/sidenav';
- 
+
+export const experimental_ppr = true;
+
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
@@ -9,6 +11,6 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
       <div className="flex-grow p-6 md:overflow-y-auto md:p-12">{children}</div>
     </div>
   );
-}
+};
 
 export default Layout;
