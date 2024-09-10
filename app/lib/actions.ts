2ef8abed6b554c555fad42e596544cf0d416d913
lanlanliang <lanlanliang@tencent.com>
2024-09-10 15:00:43 +0800

    添加类型验证

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,12 +1,24 @@
 'use server';
+import { z } from 'zod';
+
+const FormSchema = z.object({
+  id: z.string(),
+  customerId: z.string(),
+  amount: z.coerce.number(),
+  status: z.enum(['pending', 'paid']),
+  date: z.string(),
+});
+
+const CreateInvoice = FormSchema.omit({ id: true, date: true });
 
 export const createInvoice = async (formData: FormData) => {
-  // const rawFormData = {
-  //   customerId: formData.get('customerId'),
-  //   amount: formData.get('amount'),
-  //   status: formData.get('status'),
-  // };
-  const rawFormData = Object.fromEntries(formData.entries());
+  const rawFormData = CreateInvoice.parse({
+    customerId: formData.get('customerId'),
+    amount: formData.get('amount'),
+    status: formData.get('status'),
+  });
+  // Object.fromEntries() 静态方法将键值对列表转换为一个对象。
+  // const rawFormData = Object.fromEntries(formData.entries());
   console.log(rawFormData);
   /* 选择表单进行操作，最后点提交，打印结果
   {
@@ -15,4 +27,13 @@ export const createInvoice = async (formData: FormData) => {
     amount: '5',
     status: 'paid'
   }*/
+
+  /**
+     * 加完类型验证后，打印
+     * {
+        customerId: '3958dc9e-712f-4377-85e9-fec4b6a6442a',
+        amount: 123,
+        status: 'paid'
+      }
+  */
 };
