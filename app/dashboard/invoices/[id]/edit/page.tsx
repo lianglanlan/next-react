e394aa99754db54e3539451e20cd021560fd74c5
lanlanliang <lanlanliang@tencent.com>
2024-09-10 17:21:30 +0800

    获取路由地址中的id

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -1,5 +1,22 @@
-const Page = () => {
-    return <>123</>
-};
+import Form from '@/app/ui/invoices/edit-form';
+import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
+import { fetchCustomers } from '@/app/lib/data';
 
-export default Page;
+export default async function Page({ params }: { params: { id: string } }) {
+  const id = params.id;
+  return (
+    <main>
+      <Breadcrumbs
+        breadcrumbs={[
+          { label: 'Invoices', href: '/dashboard/invoices' },
+          {
+            label: 'Edit Invoice',
+            href: `/dashboard/invoices/${id}/edit`,
+            active: true,
+          },
+        ]}
+      />
+      {/* <Form invoice={invoice} customers={customers} /> */}
+    </main>
+  );
+}
