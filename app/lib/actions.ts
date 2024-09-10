b88a1602551b541778ff22c94bff09fbe17c0063
lanlanliang <lanlanliang@tencent.com>
2024-09-10 16:24:37 +0800

    写入数据库

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,5 +1,6 @@
 'use server';
 import { z } from 'zod';
+import { sql } from '@vercel/postgres';
 
 const FormSchema = z.object({
   id: z.string(),
@@ -12,14 +13,13 @@ const FormSchema = z.object({
 const CreateInvoice = FormSchema.omit({ id: true, date: true });
 
 export const createInvoice = async (formData: FormData) => {
-  const rawFormData = CreateInvoice.parse({
+  const { customerId, amount, status } = CreateInvoice.parse({
     customerId: formData.get('customerId'),
     amount: formData.get('amount'),
     status: formData.get('status'),
   });
   // Object.fromEntries() 静态方法将键值对列表转换为一个对象。
   // const rawFormData = Object.fromEntries(formData.entries());
-  console.log(rawFormData);
   /* 选择表单进行操作，最后点提交，打印结果
   {
     '$ACTION_ID_a5b783090ca6573b8898d600e86326101a894424': '',
@@ -36,4 +36,12 @@ export const createInvoice = async (formData: FormData) => {
         status: 'paid'
       }
   */
+  // 将元转化为分，避免浮点数的误差
+  const amountInCents = amount * 100;
+  // toISOString 返回  YYYY-MM-DDTHH:mm:ss.sssZ
+  const date = new Date().toISOString().split('T')[0];
+  await sql`
+    INSERT INTO invoices (customer_id, amount, status,date)
+    VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+  `;
 };
